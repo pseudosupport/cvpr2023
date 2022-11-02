0c0d29fb89fa74fb99c8fc160cc760d091396660
yyyyyyyyymmmmmmmmmm <117270881+yyyyyyyyymmmmmmmmmm@users.noreply.github.com>
2022-11-02 19:41:19 +0800

    Add files via upload

---
+++ deepbdc-new/scripts/mini_imagenet/deepbdc-new/scripts/mini_imagenet/run_protonet_draw/run_test.sh
@@ -0,0 +1,12 @@
+gpuid=4
+DATA_ROOT=/home/chenxu/mit2023/cx_work/DEEPBDC-new/filelist/miniimagenet
+
+MODEL_1SHOT_PATH=/home/chenxu/mit2023/cx_work/DeepBDC-main/checkpoints/mini_imagenet/ResNet12_protonet_5way_1shot_metatrain/best_model.tar
+MODEL_5SHOT_PATH=./checkpoints/mini_imagenet/ResNet12_protonet_1_5way_5shot_metatrain/best_model.tar
+cd ../../../
+
+N_SHOT=1
+python test_c_draw.py --dataset mini_imagenet --data_path $DATA_ROOT --model ResNet12 --method protonet --image_size 84 --gpu ${gpuid} --n_shot $N_SHOT --reduce_dim 640 --model_path $MODEL_1SHOT_PATH --test_task_nums 5 --test_n_episode 2000 --fake 1
+
+#N_SHOT=5
+#python test_c.py --dataset mini_imagenet --data_path $DATA_ROOT --model ResNet12 --method protonet --image_size 84 --gpu ${gpuid} --n_shot $N_SHOT --reduce_dim 640 --model_path $MODEL_5SHOT_PATH --test_task_nums 5 --test_n_episode 2000 --fake 1
